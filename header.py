c4ca88feab3e1a346d2c7597f41b3972e11dc8b7
Brendan Griffen <projectcaterpillar@gmail.com>
2013-07-22 10:57:24 -0400

    updated directory structure

--- header.py
+++ header.py
@@ -1,5 +1,6 @@
 from Common import *
-
+import getpass
+from os.path import expanduser
 class Header(HasTraits):
 
     view = View(Item(name='clusteropt',label='Cluster' ,padding=5),
@@ -29,55 +30,47 @@ class Header(HasTraits):
         self.musicpath = self.homepath + self.username + '/lib/music/'
 
     def _clusteropt_changed(self):
-        if self.clusteropt == 'Macbook':
-            self.username = 'griffen'
-            self.homepath = '/home/'
+        self.homepath = expanduser("~")
 
-        if self.clusteropt == 'antares':
-            self.username = 'griffen'
-            self.homepath = '/home/'
-
-        if self.clusteropt == 'barrine':
-            self.username = 'uqbgriff'
-            self.homepath = '/home/'
-
-        if self.clusteropt == 'odyssey':
-            self.username = 'bgriffen'
-            self.homepath = '/n/home01/'
-
-        if self.clusteropt == 'spacebase':
-            self.username = 'bgriffen'
-            self.homepath = '/spacebase/data/'
-
+#        if self.clusteropt == 'Macbook':
+#            self.homepath = '/home/'
+#
+#        if self.clusteropt == 'antares':
+#            self.homepath = '/home/'
+#
+#        if self.clusteropt == 'barrine':
+#            self.homepath = '/home/'
+#
+#        if self.clusteropt == 'odyssey':
+#            self.homepath = '/n/home01/'
+#
+#        if self.clusteropt == 'spacebase':
+#            self.homepath = '/spacebase/data/'
+#
         self.masterpath = self.homepath + self.username
 
     def __init__(self, main, **kwargs):
-        if platform.node() == "csr-dyn-150.mit.edu":
-            self.username = 'griffen'
-            self.homepath = '/Users/'
-        
-        if platform.node() == "Brendans-MacBook-Pro.local":
-            self.username = 'griffen'
-            self.homepath = '/Users/'
-            
+        self.username = getpass.getuser()
+        self.homepath = expanduser("~")
+#        if platform.node() == "csr-dyn-150.mit.edu":
+#            self.homepath = '/Users/'
+#        
+#        if platform.node() == "Brendans-MacBook-Pro.local":
+#            self.homepath = '/Users/'
+#            
         if platform.node() == 'antares':
             self.clusteropt = 'antares'
-            self.username = 'griffen'
-            self.homepath = '/home/'
 
         if platform.node() == 'barrine':
             self.username = 'uqbgriff'
-            self.homepath = '/home/'
 
         if platform.node() == 'odyssey':
             self.clusteropt = 'odyssey'
-            self.username = 'bgriffen'
-            self.homepath = '/n/home01/'
 
         if platform.node() == 'spacebase':
             self.clusteropt = 'spacebase'
-            self.username = 'bgriffen'
             self.homepath = '/spacebase/data/'
+            self.masterpath = self.homepath + self.username
             self.gadgetpath = self.masterpath + '/lib/P-Gadget3'
             self.musicpath = self.masterpath+ '/lib/music'
             self.datamasterpath = self.homepath + 'AnnaGroup/caterpillar/'
